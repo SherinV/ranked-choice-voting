89b657855fcb78dca68ba3ed1e5a1f80f2c3b85c
audrey-lorberfeld <audrey.lorberfeld@ibm.com>
2020-08-27 22:13:53 -0400

    Got all candidates in master dataset into single cell

--- pyrankobjs/audrey_refactor.py
+++ pyrankobjs/audrey_refactor.py
@@ -1,5 +1,6 @@
 import os
 import glob
+from typing import List
 import pandas as pd
 
 
@@ -9,7 +10,7 @@ def create_master_file_from_csvs(glob_pattern='../data/*.csv') -> pd.DataFrame()
     single master dataset using a generator
 
     :param glob_pattern: pattern for recursive glob
-    search files
+        search files
 
     :return: master dataset as dataframe
     """
@@ -21,11 +22,35 @@ def create_master_file_from_csvs(glob_pattern='../data/*.csv') -> pd.DataFrame()
             yield df
 
     master_df = pd.concat(list(yielder(glob_pattern)))
-    master_df.to_csv('master_elections.csv')
+    master_df.to_csv('./master_elections.csv', index=False)
     return master_df
 
+
+def get_cands_into_single_cell(df: pd.DataFrame()) -> List[str]:
+    """
+    Per election within master dataframe, take
+    all candidates and put them in one cell, separating
+    them with commas
+
+    :param df: master dataset
+
+    :return: list of concatenated candidates
+        per election within master dataset
+        that you can then use to set a new
+        column in master dataset called
+        "candidate_list"
+    """
+    concat_cands = []
+    for name, group in df.groupby('filename'):
+        group = group.dropna(axis=1, how='all')
+        cand_cols = [col for col in group if col.startswith('candidate_')]
+        group[cand_cols] = group[cand_cols].astype('str')
+        group['cands'] = group[cand_cols].agg(", ".join, axis=1)
+        concat_cands.extend(group['cands'].values)
+    return concat_cands
 
 
 if __name__ == "__main__":
     master_df = create_master_file_from_csvs()
-    print('hi')
+    # master_df = pd.read_csv('./master_elections.csv')  # tmp
+    master_df['candidate_list'] = get_cands_into_single_cell(master_df)
