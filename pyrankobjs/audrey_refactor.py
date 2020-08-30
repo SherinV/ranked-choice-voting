945cffa07803e5952168a4e49060d36e10acb23a
audrey-lorberfeld <audrey.lorberfeld@ibm.com>
2020-08-30 13:55:32 -0400

    Working on election_dict

--- pyrankobjs/audrey_refactor.py
+++ pyrankobjs/audrey_refactor.py
@@ -2,6 +2,8 @@ import os
 import glob
 from typing import List
 import pandas as pd
+import pyrankvote
+from pyrankvote import Candidate, Ballot
 
 
 def create_master_file_from_csvs(glob_pattern='../data/*.csv') -> pd.DataFrame():
@@ -46,16 +48,106 @@ def get_cands_into_single_cell(df: pd.DataFrame()) -> List:
     """
     group_dfs = []
     for name, group in df.groupby('filename'):
-        print(name)
         group = group.dropna(axis=1, how='all')
         cand_cols = [col for col in group if col.startswith('candidate_')]
-        group['cands'] = group[cand_cols].agg(", ".join, axis=1)
+        group['candidate_list'] = group[cand_cols].agg(", ".join, axis=1)
         group_dfs.append(group)
     return group_dfs
 
+def initialize_cand_objs(cand_list):
+    """
+    :goal: Initialize Candidate() objects from PyRankVote library
+    :param cand_list:
+    :return:
+    """
+    if isinstance(cand_list, str):
+        return [Candidate(c) for c in cand_list.split(', ')]
+    else:
+        return [Candidate(c) for c in cand_list]
+
+def initialize_ballot_objs(df):
+    """
+    :goal: initialize Ballot() objects from PyRankVote library
+    :param df:
+    :return:
+    """
+    ballot_objects = []
+    for index, value in enumerate(df['candidate_list']):
+        ballot = Ballot(ranked_candidates=value)
+        ballot_objects.append(ballot)
+    return ballot_objects
+
+
+def run_single_election(list_of_cand_objs: List, list_of_ballot_objs: List):
+    return pyrankvote.instant_runoff_voting(list_of_cand_objs,
+                                            list_of_ballot_objs,
+                                            pick_random_if_blank=True)
+
+def transform_single_election_metadata_to_dict(str):
+    list_of_rounds = [i for i in str.split('\n') if i.startswith('R') or i.startswith('F')]
+    split_into_rounds = str.split('\n\n')
+    rounds = []
+
+    # for each round (2):
+    for round in range(0, len(split_into_rounds)):
+        # isolate the candidate names (3):
+        cands = split_into_rounds[round].split('\n')[3:]
+        cands = [i.split('  ') for i in cands]
+        cand_votes = []
+        # for each candidate name, isolate the # of votes
+        # and save the name,# tuple to cand_votes
+        for cand in cands:
+            if len(cand)>1:
+                cand_name = cand[0]
+                num_votes = cand[-2]
+                cand_with_votes = (cand_name, num_votes)
+                cand_votes.append(cand_with_votes)
+        rounds.append((f'Round: {round+1}', cand_votes))
+
+    return dict(rounds)
+
+
+
+def run_all_elections(df):
+    elect_results = []
+    for name, group in df.groupby('filename'):
+        cands = group['candidate_list'].to_list()[0]
+        ballots = group['ballots'].to_list()
+        elect_result = run_single_election(cands, ballots)
+        elect_results.append((elect_result.__str__(), elect_result))
+    return elect_results
+
+
+# run election
+# get winner
+# rounds
+# spoiled/not
+# election dictionary: {'winner': [<Candidate('candidate_2')>], 'rounds': 2, 'election': <ElectionResults(2 rounds)>}
+
 
 if __name__ == "__main__":
-    master_df = create_master_file_from_csvs()
-    # master_df = pd.read_csv('./master_elections.csv')  # tmp
+    # master_df = create_master_file_from_csvs()
+    master_df = pd.read_csv('./master_elections.csv')  # tmp
     dfs_with_cands_list = get_cands_into_single_cell(master_df)
-    master_df = pd.concat([df for df in dfs_with_cands_list])
+    master_df = pd.concat([df for df in dfs_with_cands_list])  # len = 105775
+
+    # remove rows w/only 0s
+    master_df = master_df[master_df['candidate_list'] != '0']
+
+    # replacing 0s with blank space to work with pyrankvote
+    master_df['candidate_list'] = master_df['candidate_list'].apply(
+        lambda x: x.replace('0, ', '').replace(', 0', ''))
+
+    # initializing pyrankvote candidate objs
+    master_df['candidate_list'] = master_df['candidate_list'].apply(lambda x: initialize_cand_objs(x))
+
+    # initializing ballot objs
+    master_df['ballots'] = initialize_ballot_objs(master_df)
+
+    all_election_metadata = run_all_elections(master_df)  # metadata + election result
+
+    # first election, metadata
+    test = transform_single_election_metadata_to_dict(all_election_metadata[0][0])
+
+
+    print('hi')
