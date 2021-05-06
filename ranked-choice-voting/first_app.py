ce0df6159f8c5b41c9865722d3aee9337ed06bc3
audreylorberfeld <aulorbe@gmail.com>
2021-05-06 15:17:11 -0400

    testing out streamlit

---
+++ ranked-choice-voting/first_app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+
+st.title("my first app")
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
