9d42ae2f277a1f381e2978f4d6510558701f3acc
audreylorberfeld <aulorbe@gmail.com>
2021-05-06 15:54:22 -0400

    adding reqs for deploying app

--- ranked-choice-voting/first_app.py
+++ ranked-choice-voting/first_app.py
@@ -2,55 +2,54 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import time
+from templates.s3_stuff import open_s3_connections, download_csv_from_s3_and_load_as_df
 
 st.title("How Likely is the Spoiler Effect in Ranked Choice Voting Systems?")
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+s3_connection = open_s3_connections()
 
-df
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
 
-st.line_chart(chart_data)
 
-'Starting a long computation...'
-
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
-
-'...and now we\'re done!'
-
-progress_bar = st.progress(0)
-status_text = st.empty()
-chart = st.line_chart(np.random.randn(10, 2))
-
-for i in range(100):
-    # Update progress bar.
-    progress_bar.progress(i + 1)
-
-    new_rows = np.random.randn(10, 2)
-
-    # Update status text.
-    status_text.text(
-        'The latest random number is: %s' % new_rows[-1, 1])
-
-    # Append data to the chart.
-    chart.add_rows(new_rows)
-
-    # Pretend we're doing some computation that takes time.
-    time.sleep(0.1)
-
-status_text.text('Done!')
-st.balloons()
+# chart_data = pd.DataFrame(
+#      np.random.randn(20, 3),
+#      columns=['a', 'b', 'c'])
+#
+# st.line_chart(chart_data)
+#
+# 'Starting a long computation...'
+#
+# # Add a placeholder
+# latest_iteration = st.empty()
+# bar = st.progress(0)
+#
+# for i in range(100):
+#   # Update the progress bar with each iteration.
+#   latest_iteration.text(f'Iteration {i+1}')
+#   bar.progress(i + 1)
+#   time.sleep(0.1)
+#
+# '...and now we\'re done!'
+#
+# progress_bar = st.progress(0)
+# status_text = st.empty()
+# chart = st.line_chart(np.random.randn(10, 2))
+#
+# for i in range(100):
+#     # Update progress bar.
+#     progress_bar.progress(i + 1)
+#
+#     new_rows = np.random.randn(10, 2)
+#
+#     # Update status text.
+#     status_text.text(
+#         'The latest random number is: %s' % new_rows[-1, 1])
+#
+#     # Append data to the chart.
+#     chart.add_rows(new_rows)
+#
+#     # Pretend we're doing some computation that takes time.
+#     time.sleep(0.1)
+#
+# status_text.text('Done!')
+# st.balloons()
