9d98778dfdb05233b8daa899eeee3037f8a36674
audreylorberfeld <aulorbe@gmail.com>
2021-05-06 16:31:03 -0400

    working

--- first_app.py
+++ first_app.py
@@ -2,21 +2,36 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import time
+from sklearn.ensemble import RandomForestClassifier as rf
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 from templates.s3_stuff import open_s3_connections, download_csv_from_s3_and_load_as_df
 
 st.title("How Likely is the Spoiler Effect in Ranked Choice Voting Systems?")
 
 s3_connection = open_s3_connections()
 
+data = download_csv_from_s3_and_load_as_df(s3_connection, file_name='final_master.csv')
+data = data.replace(np.nan, 0)
+
+X = data.iloc[:, :-1].values
+y = data['spoiled'].values
+
+train_X, test_X, train_y, test_y = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+rf_model = rf(n_estimators=1000, random_state=42)
+
+"training model..."  # TODO: get code for generating moving bar for how long this takes
+rf_model.fit(train_X, train_y)  # TODO: figure out how to cache this or skip it altogether for same user
+
+predictions = rf_model.predict(test_X)
+
+conf_matrix = confusion_matrix(test_y, predictions, normalize=True)
+
+st.write(conf_matrix)
 
 
-
-# chart_data = pd.DataFrame(
-#      np.random.randn(20, 3),
-#      columns=['a', 'b', 'c'])
-#
-# st.line_chart(chart_data)
-#
 # 'Starting a long computation...'
 #
 # # Add a placeholder
