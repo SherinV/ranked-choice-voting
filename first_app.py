152bd6f75dd3a1ddd7c2c25c2fa47f0b5c8a53cf
audreylorberfeld <aulorbe@gmail.com>
2021-05-10 21:18:08 -0400

    final touches before meeting tomorrow

--- first_app.py
+++ first_app.py
@@ -11,35 +11,39 @@ s3_connection = open_s3_connections()
 st.subheader("How many elections do you want to produce?")
 num_elections = st.text_input('Number of elections',  value=1)
 
-next = st.checkbox("Check to continue", key='1')
-if next:
+cont = st.checkbox("Check to continue", key='1')
+if cont:
     if int(num_elections) == 1:
         num_cands = st.select_slider('Select the number of candidates for your election', options=[3, 4, 5, 6, 7, 8], value=3)
-        next_2 = st.checkbox("Check to continue", key='2')
-        if next_2:
+        cont = st.checkbox("Check to continue", key='2')
+        if cont:
             amt_noise = st.select_slider('Select the amount of noise in your election', options=list(np.arange(0, 16, .5)), value=0.0)
 
-            next_4 = st.checkbox("Check to create your election", key='4')  # TODO: make progress bar
-            if next_4:
+            cont = st.checkbox("Check to create your election", key='3')  # TODO: make progress bar
+            if cont:
                 df = create_dataset_for_modeling(1, user_input=[num_cands, amt_noise])
                 st.dataframe(df)
     else:
         st.subheader("Range of candidates:")
         # TODO: figure out way to prevent same number being the min and max on the slider below
-        num_cands = st.slider('Select a range representing the number of candidates each election in your dataset can have. Range is inclusive.',
+        num_cands = st.slider('Select a range representing the number of candidates each election in your dataset'
+                              ' can have. Range is inclusive.',
                               3, 8, (3, 8))
-        next_3 = st.checkbox("Check to continue", key='3')
-        if next_3:
+        cont = st.checkbox("Check to continue", key='4')
+        if cont:
             st.subheader("Range of noise:")
-            amt_noise = st.slider('Select a range representing the amount of noise each election in your dataset can have. Range is inclusive.',
+            amt_noise = st.slider('Select a range representing the amount of noise each election in your dataset '
+                                  'can have. Range is inclusive.',
                                   0, 15, (0, 15))
-            next_5 = st.checkbox("Check to create your election", key='5')  # TODO: make progress bar
-            if next_5:
+            cont = st.checkbox("Check to create your election", key='5')  # TODO: make progress bar
+            if cont:
                 df = create_dataset_for_modeling(int(num_elections), user_input=[num_cands, amt_noise])
                 st.dataframe(df)
 
 
 # TODO: include something re: # of ballots in each election that are auto-generated
+# TODO: include legend for reading election(s) dataframe
+# TODO: include styling for election(s) dataframe
 
 
 # with st.spinner('Fetching pretrained model from S3...'):
